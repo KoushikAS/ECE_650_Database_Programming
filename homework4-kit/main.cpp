5e06148f1e7b0249a48da616382ad5e756e4bf67
Koushik Annareddy Sreenath <koushik.annareddysreenath@duke.edu>
2023-03-15 15:59:46 -0400

    Adding inital state and color

--- homework4-kit/main.cpp
+++ homework4-kit/main.cpp
@@ -1,44 +1,82 @@
+#include <fstream>
 #include <iostream>
 #include <pqxx/pqxx>
+#include <sstream>
+#include <string>
 
 #include "exerciser.h"
+#include "query_funcs.h"
 
 using namespace std;
 using namespace pqxx;
 
-int main (int argc, char *argv[]) 
-{
+void readState(connection * C) {
+  fstream state_file;
+  state_file.open("state.txt", ios::in);
+  if (state_file.is_open()) {
+    string line, word;
+    while (getline(state_file, line)) {
+      stringstream s(line);
+      getline(s, word, ' ');
+      int id = stoi(word);
+      getline(s, word, ' ');
+      string name = word;
 
+      add_state(C, name);
+    }
+    state_file.close();
+  }
+}
+
+void readColor(connection * C) {
+  fstream color_file;
+  color_file.open("color.txt", ios::in);
+  if (color_file.is_open()) {
+    string line, word;
+    while (getline(color_file, line)) {
+      stringstream s(line);
+      getline(s, word, ' ');
+      int id = stoi(word);
+      getline(s, word, ' ');
+      string name = word;
+
+      add_color(C, name);
+    }
+    color_file.close();
+  }
+}
+
+int main(int argc, char * argv[]) {
   //Allocate & initialize a Postgres connection object
-  connection *C;
+  connection * C;
 
-  try{
+  try {
     //Establish a connection to the database
     //Parameters: database name, user name, user password
     C = new connection("dbname=ACC_BBALL user=postgres password=passw0rd");
     if (C->is_open()) {
       cout << "Opened database successfully: " << C->dbname() << endl;
-    } else {
+    }
+    else {
       cout << "Can't open database" << endl;
       return 1;
     }
-  } catch (const std::exception &e){
+  }
+  catch (const std::exception & e) {
     cerr << e.what() << std::endl;
     return 1;
   }
 
-
+  initalize_db(C);
+  readState(C);
+  readColor(C);
   //TODO: create PLAYER, TEAM, STATE, and COLOR tables in the ACC_BBALL database
   //      load each table with rows from the provided source txt files
 
-
   exercise(C);
-
 
   //Close database connection
   C->disconnect();
 
   return 0;
 }
-
-
--- homework4-kit/query_funcs.cpp
+++ homework4-kit/query_funcs.cpp
@@ -1,24 +1,63 @@
 #include "query_funcs.h"
 
+#include <pqxx/pqxx>
 
-void add_player(connection *C, int team_id, int jersey_num, string first_name, string last_name,
-                int mpg, int ppg, int rpg, int apg, double spg, double bpg)
-{
+void initalize_db(connection * C) {
+  char * sql =
+      "DROP TABLE IF EXISTS COLOR, STATE, TEAM, PLAYER;"
+      "CREATE TABLE COLOR(COLOR_ID SERIAL NOT NULL, NAME TEXT "
+      "NOT NULL, PRIMARY KEY(COLOR_ID));"
+      "CREATE TABLE STATE(STATE_ID SERIAL NOT NULL, NAME TEXT "
+      "NOT NULL, PRIMARY KEY(STATE_ID));"
+      "CREATE TABLE TEAM(TEAM_ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, STATE_ID "
+      "INT NOT NULL, COLOR_ID INT NOT NULL, WINS INT, LOOSES INT, CONSTRAINT "
+      "FK_STATE_TEAM FOREIGN "
+      "KEY(STATE_ID) REFERENCES STATE(STATE_ID) ON DELETE CASCADE,  CONSTRAINT "
+      "FK_COLOR_TEAM FOREIGN "
+      "KEY(COLOR_ID) REFERENCES COLOR(COLOR_ID) ON DELETE CASCADE);"
+      "CREATE TABLE PLAYER(PLAYER_ID INT PRIMARY KEY NOT NULL, TEAM_ID INT NOT NULL, "
+      "UNIFORM_NUM INT, FIRST_NAME TEXT, LAST_NAME TEXT, MPG INT, PPG INT, RPG INT, ARG "
+      "INT, SPG INT, BPG INT, CONSTRAINT FK_TEAM_PLAYER FOREIGN "
+      "KEY(TEAM_ID) REFERENCES TEAM(TEAM_ID) ON DELETE CASCADE);";
+
+  work W(*C);
+  W.exec(sql);
+  W.commit();
 }
 
-
-void add_team(connection *C, string name, int state_id, int color_id, int wins, int losses)
-{
+void add_player(connection * C,
+                int team_id,
+                int jersey_num,
+                string first_name,
+                string last_name,
+                int mpg,
+                int ppg,
+                int rpg,
+                int apg,
+                double spg,
+                double bpg) {
 }
 
-
-void add_state(connection *C, string name)
-{
+void add_team(connection * C,
+              string name,
+              int state_id,
+              int color_id,
+              int wins,
+              int losses) {
 }
 
+void add_state(connection * C, string name) {
+  work W(*C);
+  C->prepare("insert_state", "INSERT INTO STATE (NAME) VALUES ($1)");
+  W.prepared("insert_state")(name).exec();
+  W.commit();
+}
 
-void add_color(connection *C, string name)
-{
+void add_color(connection * C, string name) {
+  work W(*C);
+  C->prepare("insert_color", "INSERT INTO COLOR (NAME) VALUES ($1)");
+  W.prepared("insert_color")(name).exec();
+  W.commit();
 }
 
 /*
@@ -26,33 +65,35 @@ void add_color(connection *C, string name)
  * a 1 for a use_ param means this attribute is enabled (i.e. a WHERE clause is needed)
  * a 0 for a use_ param means this attribute is disabled
  */
-void query1(connection *C,
-	    int use_mpg, int min_mpg, int max_mpg,
-            int use_ppg, int min_ppg, int max_ppg,
-            int use_rpg, int min_rpg, int max_rpg,
-            int use_apg, int min_apg, int max_apg,
-            int use_spg, double min_spg, double max_spg,
-            int use_bpg, double min_bpg, double max_bpg
-            )
-{
+void query1(connection * C,
+            int use_mpg,
+            int min_mpg,
+            int max_mpg,
+            int use_ppg,
+            int min_ppg,
+            int max_ppg,
+            int use_rpg,
+            int min_rpg,
+            int max_rpg,
+            int use_apg,
+            int min_apg,
+            int max_apg,
+            int use_spg,
+            double min_spg,
+            double max_spg,
+            int use_bpg,
+            double min_bpg,
+            double max_bpg) {
 }
 
-
-void query2(connection *C, string team_color)
-{
+void query2(connection * C, string team_color) {
 }
 
-
-void query3(connection *C, string team_name)
-{
+void query3(connection * C, string team_name) {
 }
 
-
-void query4(connection *C, string team_state, string team_color)
-{
+void query4(connection * C, string team_state, string team_color) {
 }
 
-
-void query5(connection *C, int num_wins)
-{
+void query5(connection * C, int num_wins) {
 }
